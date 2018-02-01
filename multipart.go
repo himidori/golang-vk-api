ab3c9da5612f6500778aa5adbf2e2ac40d5f58a4
himidori <himidori1@yandex.ru>
2018-02-01 04:29:34 +0300

    Different methods for getting docs and photos upload requests

--- multipart.go
+++ multipart.go
@@ -3,7 +3,6 @@ package vkapi
 import (
 	"bytes"
 	"errors"
-	"fmt"
 	"io"
 	"mime/multipart"
 	"net/http"
@@ -31,20 +30,18 @@ func getFilesSizeMB(files []string) (int, error) {
 	return int(size / 1048576), nil
 }
 
-func (client *VKClient) getMultipartRequest(link string, fileType string, files []string) (*http.Request, error) {
+func (client *VKClient) getPhotoMultipartReq(link string, files []string) (*http.Request, error) {
 	if len(files) > 6 {
-		return nil, errors.New("you can't upload more than 6 files")
+		return nil, errors.New("you can't upload more than 6 photos")
 	}
 
 	size, err := getFilesSizeMB(files)
 	if err != nil {
 		return nil, err
 	}
-
 	if size > 50 {
-		return nil, errors.New("summary files size can't be higher than 50mb")
+		return nil, errors.New("summary photos size can't be higher than 50mb")
 	}
-	fmt.Println("size", size)
 
 	body := &bytes.Buffer{}
 	writer := multipart.NewWriter(body)
@@ -69,6 +66,50 @@ func (client *VKClient) getMultipartRequest(link string, fileType string, files 
 		file.Close()
 		idx++
 	}
+
+	err = writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", link, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Content-Type", writer.FormDataContentType())
+
+	return req, nil
+
+}
+
+func (client *VKClient) getDocMultipartReq(link string, fileName string) (*http.Request, error) {
+	size, err := getFilesSizeMB([]string{fileName})
+	if err != nil {
+		return nil, err
+	}
+	if size > 200 {
+		return nil, errors.New("document size can't be higher than 200mb")
+	}
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	fi, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	part, err := writer.CreateFormFile("file", fi.Name())
+	if err != nil {
+		return nil, err
+	}
+	io.Copy(part, file)
 
 	err = writer.Close()
 	if err != nil {
