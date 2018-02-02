cdbec9b3a0c5e202de545038db78938f291ac28b
himidori <himidori1@yandex.ru>
2018-02-02 04:19:34 +0300

    Method to post comments under group's wall post

--- wall.go
+++ wall.go
@@ -62,13 +62,13 @@ func (client *VKClient) GetWallPosts(id interface{}, count int, params url.Value
 		params = url.Values{}
 	}
 
-	params.Add("count", strconv.Itoa(count))
+	params.Set("count", strconv.Itoa(count))
 
 	switch id.(type) {
 	case int:
-		params.Add("owner_id", strconv.Itoa(id.(int)))
+		params.Set("owner_id", strconv.Itoa(id.(int)))
 	case string:
-		params.Add("domain", id.(string))
+		params.Set("domain", id.(string))
 	}
 
 	resp, err := client.makeRequest("wall.get", params)
@@ -86,8 +86,8 @@ func (client *VKClient) WallPost(ownerID int, message string, params url.Values)
 	if params == nil {
 		params = url.Values{}
 	}
-	params.Add("owner_id", strconv.Itoa(ownerID))
-	params.Add("message", message)
+	params.Set("owner_id", strconv.Itoa(ownerID))
+	params.Set("message", message)
 
 	resp, err := client.makeRequest("wall.post", params)
 	if err != nil {
@@ -99,4 +99,25 @@ func (client *VKClient) WallPost(ownerID int, message string, params url.Values)
 	}
 
 	return m["post_id"], nil
+}
+
+func (client *VKClient) WallPostComment(ownerID int, postID int, message string, params url.Values) (int, error) {
+	if params == nil {
+		params = url.Values{}
+	}
+	params.Set("owner_id", strconv.Itoa(ownerID))
+	params.Set("post_id", strconv.Itoa(postID))
+	params.Set("message", message)
+
+	resp, err := client.makeRequest("wall.createComment", params)
+	if err != nil {
+		return 0, err
+	}
+	m := map[string]int{}
+	if err = json.Unmarshal(resp.Response, &m); err != nil {
+		return 0, err
+	}
+
+	return m["comment_id"], nil
+
 }
