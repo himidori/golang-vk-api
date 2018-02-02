fc6f4bae398b1a59d42ba75b67fa2c42ba448ac1
himidori <himidori1@yandex.ru>
2018-02-02 14:01:44 +0300

    Methods to search and get info about groups

--- group.go
+++ group.go
@@ -1,11 +1,50 @@
 package vkapi
 
 import (
+	"encoding/json"
 	"net/url"
 	"strconv"
 )
 
-func (client *VKClient) SendGroupInvite(groupID int, userID int) error {
+var (
+	fields = "description,age_limits,activity,can_create_topic," +
+		"can_message,can_post,can_see_all_posts,contacts,has_photo," +
+		"is_messages_blocked"
+)
+
+type Group struct {
+	ID                int    `json:"id"`
+	Name              string `json:"name"`
+	ScreenName        string `json:"screen_name"`
+	Description       string `json:"description"`
+	Activity          string `json:"activity"`
+	IsClosed          int    `json:"is_closed"`
+	Type              string `json:"type"`
+	IsAdmin           int    `json:"is_admin"`
+	IsMember          int    `json:"is_member"`
+	HasPhoto          int    `json:"has_photo"`
+	IsMessagesBlocked int    `json:"is_messages_blocked"`
+	Photo50           string `json:"photo_50"`
+	Photo100          string `json:"photo_100"`
+	Photo200          string `json:"photo_200"`
+	AgeLimit          int    `json:"age_limits"`
+	CanCreateTopic    int    `json:"can_create_topic"`
+	CanMessage        int    `json:"can_message"`
+	CanPost           int    `json:"can_post"`
+	CanSeeAllPosts    int    `json:"can_see_all_posts"`
+}
+
+type GroupSearchResult struct {
+	Count  int      `json:"count"`
+	Groups []*Group `json:"items"`
+}
+
+type GroupContact struct {
+	UID         int    `json:"user_id"`
+	Description string `json:"desc"`
+}
+
+func (client *VKClient) GroupSendInvite(groupID int, userID int) error {
 	params := url.Values{}
 	params.Set("group_id", strconv.Itoa(groupID))
 	params.Set("user_id", strconv.Itoa(userID))
@@ -15,4 +54,34 @@ func (client *VKClient) SendGroupInvite(groupID int, userID int) error {
 	}
 
 	return nil
+}
+
+func (client *VKClient) GroupSearch(query string, count int) (*GroupSearchResult, error) {
+	params := url.Values{}
+	params.Set("q", query)
+	params.Set("count", strconv.Itoa(count))
+	params.Set("fields", fields)
+	resp, err := client.makeRequest("groups.search", params)
+	if err != nil {
+		return nil, err
+	}
+
+	var res *GroupSearchResult
+	json.Unmarshal(resp.Response, &res)
+	return res, nil
+}
+
+func (client *VKClient) GroupGet(userID int, count int) (*GroupSearchResult, error) {
+	params := url.Values{}
+	params.Set("user_id", strconv.Itoa(userID))
+	params.Set("count", strconv.Itoa(count))
+	params.Set("extended", "1")
+	resp, err := client.makeRequest("groups.get", params)
+	if err != nil {
+		return nil, err
+	}
+
+	var res *GroupSearchResult
+	json.Unmarshal(resp.Response, &res)
+	return res, nil
 }
