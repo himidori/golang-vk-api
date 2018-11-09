4ec093567a7b519877586c2809563446a7f75dcd
Alexander Sheiko <alexander.sheiko@mgid.com>
2018-11-09 10:06:31 +0200

    Add ResolveScreenName method

--- utils.go
+++ utils.go
@@ -1,9 +1,30 @@
 package vkapi
 
 import (
+	"encoding/json"
+	"net/url"
 	"strconv"
 	"strings"
 )
+
+type ResolveScreenName struct {
+	Type     string `json:"type"`
+	ObjectID int    `json:"object_id"`
+}
+
+func (client *VKClient) ResolveScreenName(name string) (ResolveScreenName, error) {
+	var res ResolveScreenName
+	params := url.Values{}
+	params.Set("screen_name", name)
+
+	resp, err := client.makeRequest("utils.resolveScreenName", params)
+	if err == nil {
+
+		json.Unmarshal(resp.Response, &res)
+	}
+	return res, err
+
+}
 
 func ArrayToStr(a []int) string {
 	s := []string{}
