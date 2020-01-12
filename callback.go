5d0be18f964585ddd7780d7b5b67b0cb96251303
sanvew <sanvew@gmail.com>
2020-01-13 00:37:51 +0300

    added events handling from Bots Long Poll Server

--- callback.go
+++ callback.go
@@ -10,6 +10,11 @@ type callbackHandler struct {
 	events map[string]func(*LongPollMessage)
 }
 
+type botsCallBackHandler struct {
+	sync.Mutex
+	events map[string]func(*BotsLongPollObject)
+}
+
 func (client *VKClient) AddLongpollCallback(name string, f func(*LongPollMessage)) {
 	name = strings.ToLower(name)
 	client.cb.Lock()
@@ -29,6 +34,34 @@ func (client *VKClient) DeleteLongpollCallback(name string) {
 }
 
 func (client *VKClient) handleCallback(name string, msg *LongPollMessage) {
+	client.cb.Lock()
+	_, exists := client.cb.events[name]
+	client.cb.Unlock()
+
+	if exists {
+		client.cb.events[name](msg)
+	}
+}
+
+func (client *VKGroupBot) AddBotsLongpollCallback(name string, f func(*BotsLongPollObject)) {
+	name = strings.ToLower(name)
+	client.cb.Lock()
+	if _, exists := client.cb.events[name]; !exists {
+		client.cb.events[name] = f
+	}
+	client.cb.Unlock()
+}
+
+func (client *VKGroupBot) DeleteBotsLongpollCallback(name string) {
+	name = strings.ToLower(name)
+	client.cb.Lock()
+	if _, exists := client.cb.events[name]; exists {
+		delete(client.cb.events, name)
+	}
+	client.cb.Unlock()
+}
+
+func (client *VKGroupBot) handleBotsCallback(name string, msg *BotsLongPollObject) {
 	client.cb.Lock()
 	_, exists := client.cb.events[name]
 	client.cb.Unlock()
