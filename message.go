c06e6492779568fb989d9d62259942e0ef2ef7a8
Boris Stelmakh <mikechip@pm.me>
2020-04-23 00:17:29 +0300

    added bot keyboard support

--- message.go
+++ message.go
@@ -135,6 +135,17 @@ type LinkAttachment struct {
 	Target      string `json:"target"`
 }
 
+type Keyboard struct {
+	OneTime 	bool		`json:"one_time"`
+	Buttons		[][]Button 	`json:"buttons"`
+	Inline		bool 		`json:"inline"`
+}
+
+type Button struct {
+	Action		map[string]string	`json:"action"`
+	Color		string				`json:"color"`
+}
+
 func (client *VKClient) DialogsGet(count int, params url.Values) (*Dialog, error) {
 	if params == nil {
 		params = url.Values{}
